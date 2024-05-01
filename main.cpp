ff87d8c23ff1fb34235ad963764d0ff1997cdeb2
andrey <naumov.a.n@icloud.com>
2024-05-01 13:15:23 +0700

    lesson15 chapter3 шаблоны (строка)

--- main.cpp
+++ main.cpp
@@ -3,36 +3,26 @@
 
 using namespace std;
 
-template<typename T1, typename T2>
-T2 maxNum(T1 a, T2 b)
-{
-    return a > b ? a : b;
-}
-
 template<typename T>
-T maxNum(const vector<T> &v)
+struct Message
 {
-    if(v.size() == 0)
+    char *data;
+
+    Message(char *inData): data(inData)
     {
-        throw invalid_argument("'v' can`t be empty!");
+
     }
-    T m = v[0];
-    for(int i = 0; i < v.size(); ++i)
-    {
-        if(v[i] > m)
-        {
-            m = v[i];
-        }
+
+    void print()const{
+        cout << data << endl;
     }
-    return m;
-}
+};
 
 int main()
 {
     setlocale(LC_ALL, "rus");
-
-    vector<double> test = {10.5, 8.4, 3.4};
-    cout << "result: " <<  maxNum(test) << endl;
+    Message<string> message("hell");
+    message.print();
 
     return 0;
 }
