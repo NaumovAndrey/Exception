be5991d5bdfa23c100589a1ca6fd2c42b78f4461
andrey <naumov.a.n@icloud.com>
2024-05-01 17:14:49 +0700

    hw task1 метод добавления в базу данных

--- main.cpp
+++ main.cpp
@@ -1,28 +1,67 @@
 #include <iostream>
 #include "vector"
+#include <map>
+#include <string>
 
 using namespace std;
 
-template<typename T>
-struct Message
+map<string, int> store_db;
+
+class CheckingPositiveNumberException: public exception
 {
-    char *data;
-
-    Message(char *inData): data(inData)
+public:
+    const char *what() const noexcept override
     {
-
-    }
-
-    void print()const{
-        cout << data << endl;
+        return "The number of products must be greater than zero"; //значение <= 0
     }
 };
+
+bool addStore_db()
+{
+    string item;
+    int quantity;
+
+    cout << "Article: ";
+    getline(cin, item);
+    if(item.empty())
+    {
+        return true;
+    }
+    cout << "Quantity: ";
+    cin >> quantity;
+    cin.ignore();
+    if(quantity <= 0)
+    {
+        throw CheckingPositiveNumberException();
+
+    }
+    store_db[item] = quantity;
+
+    return false;
+}
 
 int main()
 {
     setlocale(LC_ALL, "rus");
-    Message<string> message("hell");
-    message.print();
+
+    // Заполнение базы данных магазина
+
+    cout << "Enter the article and the number of products in the store (enter an empty line to complete the input):"
+         << endl; //Введите артикул и количество товаров в магазине (введите пустую строку для завершения ввода).
+    while(true)
+    {
+        try
+        {
+            if (addStore_db()) break;
+        }
+        catch(const CheckingPositiveNumberException &e)
+        {
+            cerr << "Exception: " << e.what() << endl;
+
+        }
+    }
+
+
 
     return 0;
 }
